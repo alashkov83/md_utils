31d9f1feff45c3f9db8be89f1b63f5ce4b215766
alashkov83 <alashkov83@gmail.com>
2017-02-03 17:09:13 +0300

    Refactoring

--- occucheck2_tk.py
+++ occucheck2_tk.py
@@ -19,137 +19,144 @@ import Bio.PDB as pdb
 from Bio.PDB.mmtf import MMTFParser
 
 
-def open_pdb():
-    global structure
-    parser = pdb.PDBParser()
-    opt = {'filetypes': [
-        ('Файлы PDB', ('.pdb', '.PDB', '.ent')), ('Все файлы', '.*')]}
-    pdb_f = askopenfilename(**opt)
-    if pdb_f:
+class Gui(tk.Tk):
+    """ГУЙ"""
+
+    def __init__(self):
+        super().__init__()
+        self.title('Occucheck')
+        self.resizable(False, False)
+        self.protocol('WM_DELETE_WINDOW', self.close_win)
+        fra1 = ttk.Frame(self)
+        fra1.pack(fill=tk.X)
+        self.var1 = tk.IntVar()
+        sca1 = tk.Scale(fra1, orient='horizontal', length=560, from_=10,
+                        to=100, tickinterval=5, resolution=1, variable=self.var1)
+        sca1.grid(row=1, column=0, padx=5)
+        lab1 = ttk.Label(fra1, text='Минимальная сумма заселенности (%):')
+        lab1.grid(row=0, column=0, sticky='W', padx=5)
+        self.tx = tk.Text(self, width=80, height=10)
+        scr = ttk.Scrollbar(self, command=self.tx.yview)
+        self.tx.configure(yscrollcommand=scr.set, state='disabled')
+        self.tx.pack(side=tk.LEFT)
+        scr.pack(side=tk.RIGHT, fill=tk.Y)
+        self.menu()
+
+    def menu(self):
+        m = tk.Menu(self)  # создается объект Меню на главном окне
+        self.config(menu=m)  # окно конфигурируется с указанием меню для него
+        fm = tk.Menu(m)  # создается пункт меню с размещением на основном меню (m)
+        # пункту располагается на основном меню (m)
+        m.add_cascade(label='Файл', menu=fm)
+        # формируется список команд пункта меню
+        fm.add_command(label='Открыть PDB', command=self.open_pdb)
+        fm.add_command(label='Открыть CIF', command=self.open_cif)
+        fm.add_command(label='Открыть ID PDB', command=self.open_url)
+        fm.add_command(label='Сохранить LOG', command=self.save_log)
+        fm.add_command(label='Выход', command=self.close_win)
+        m.add_command(label='Запуск...', command=self.check_pdb)
+        m.add_command(label='Справка', command=self.about)
+
+    def close_win(self):
+        if askyesno('Выход', 'Вы точно хотите выйти?'):
+            self.destroy()
+
+    @staticmethod
+    def about():
+        showinfo('Информация',
+                 'Проверка корректности суммы заселенностей атомов а.о.')
+
+
+class App(Gui):
+    """Класс логики работы программы"""
+
+    def __init__(self):
+        super().__init__()
+        self.structure = None
+
+    def open_pdb(self):
+        parser = pdb.PDBParser()
+        opt = {'filetypes': [
+            ('Файлы PDB', ('.pdb', '.PDB', '.ent')), ('Все файлы', '.*')]}
+        pdb_f = askopenfilename(**opt)
+        if pdb_f:
+            try:
+                self.structure = parser.get_structure('X', pdb_f)
+            except FileNotFoundError:
+                return
+            except (KeyError, ValueError):
+                showerror('Ошибка!', 'Некорректный PDB файл: {0:s}!'.format(pdb_f))
+                return
+            else:
+                showinfo('Информация', 'Файл прочитан!')
+
+    def open_url(self):
+        url = askstring('Загрузить', 'ID PDB:')
+        if url is not None:
+            try:
+                self.structure = MMTFParser.get_structure_from_url(url)
+            except Exception:
+                showerror('Ошибка!', ('ID PDB: {0:s} не найден'
+                                      ' или ссылается на некорректный файл!').format(url))
+            else:
+                showinfo('Информация', 'Файл загружен!')
+
+    def open_cif(self):
+        parser = pdb.MMCIFParser()
+        opt = {'filetypes': [
+            ('Файлы mmCIF', ('.cif', '.CIF')), ('Все файлы', '.*')]}
+        cif_f = askopenfilename(**opt)
+        if cif_f:
+            try:
+                self.structure = parser.get_structure('X', cif_f)
+            except FileNotFoundError:
+                return
+            except (KeyError, ValueError, AssertionError):
+                showerror('Ошибка!', 'Некорректный CIF файл: {0:s}!'.format(cif_f))
+                return
+            else:
+                showinfo('Информация', 'Файл прочитан!')
+
+    def save_log(self):
+        sa = asksaveasfilename()
+        if sa:
+            letter = self.tx.get(1.0, tk.END)
+            try:
+                with open(sa, 'w') as f:
+                    f.write(letter)
+            except FileNotFoundError:
+                return
+
+    def check_pdb(self):
+        min_ocu = (self.var1.get()) / 100
         try:
-            structure = parser.get_structure('X', pdb_f)
-        except FileNotFoundError:
+            atoms = self.structure.get_atoms()
+        except AttributeError:
+            showerror('Ошибка!', 'Структура не загружена!')
             return
-        except (KeyError, ValueError):
-            showerror('Ошибка!', 'Некорректный PDB файл: {0:s}!'.format(pdb_f))
-            return
-        else:
-            showinfo('Информация', 'Файл прочитан!')
-
-
-def open_url():
-    global structure
-    url = askstring('Загрузить', 'ID PDB:')
-    if url is not None:
-        try:
-            structure = MMTFParser.get_structure_from_url(url)
-        except Exception:
-            showerror('Ошибка!', ('ID PDB: {0:s} не найден'
-                                  ' или ссылается на некорректный файл!').format(url))
-        else:
-            showinfo('Информация', 'Файл загружен!')
-
-
-def open_cif():
-    global structure
-    parser = pdb.MMCIFParser()
-    opt = {'filetypes': [
-        ('Файлы mmCIF', ('.cif', '.CIF')), ('Все файлы', '.*')]}
-    cif_f = askopenfilename(**opt)
-    if cif_f:
-        try:
-            structure = parser.get_structure('X', cif_f)
-        except FileNotFoundError:
-            return
-        except (KeyError, ValueError, AssertionError):
-            showerror('Ошибка!', 'Некорректный CIF файл: {0:s}!'.format(cif_f))
-            return
-        else:
-            showinfo('Информация', 'Файл прочитан!')
-
-
-def save_log():
-    sa = asksaveasfilename()
-    if sa:
-        letter = tx.get(1.0, tk.END)
-        try:
-            with open(sa, 'w') as f:
-                f.write(letter)
-        except FileNotFoundError:
-            return
-
-
-def close_win():
-    if askyesno('Выход', 'Вы точно хотите выйти?'):
-        root.destroy()
-
-
-def about():
-    showinfo('Информация',
-             'Проверка корректности суммы заселенностей атомов а.о.')
-
-
-def check_pdb():
-    min_ocu = (var1.get()) / 100
-    try:
-        atoms = structure.get_atoms()
-    except NameError:
-        showerror('Ошибка!', 'Структура не загружена!')
-        return
-    tx.configure(state='normal')
-    tx.delete(1.0, tk.END)
-    tx.configure(state='disabled')
-    for atom in atoms:
-        sum_ocu = 0.0
-        if atom.is_disordered() == 0:
-            sum_ocu += atom.get_occupancy()
-        else:
-            for X in atom.disordered_get_id_list():
-                atom.disordered_select(X)
+        self.tx.configure(state='normal')
+        self.tx.delete(1.0, tk.END)
+        self.tx.configure(state='disabled')
+        for atom in atoms:
+            sum_ocu = 0.0
+            if atom.is_disordered() == 0:
                 sum_ocu += atom.get_occupancy()
-        if (sum_ocu > 1.00) or (sum_ocu < min_ocu):
-            tx.configure(state='normal')
-            tx.insert(tk.INSERT, ('Для атома {0:s} а.о. {1:s}:{2:4d} цепи {3:s}'
-                                  ' cумма заселенностей равна {4:.2f}\n').format(
-                atom.get_fullname(), (atom.get_parent()).get_resname(), int(
-                    atom.get_full_id()[3][1]), atom.get_full_id()[2], sum_ocu))
-            tx.configure(state='disabled')
+            else:
+                for X in atom.disordered_get_id_list():
+                    atom.disordered_select(X)
+                    sum_ocu += atom.get_occupancy()
+            if (sum_ocu > 1.00) or (sum_ocu < min_ocu):
+                self.tx.configure(state='normal')
+                self.tx.insert(tk.INSERT, ('Для атома {0:s} а.о. {1:s}:{2:4d} цепи {3:s}'
+                                           ' cумма заселенностей равна {4:.2f}\n').format(
+                    atom.get_fullname(), (atom.get_parent()).get_resname(), int(
+                        atom.get_full_id()[3][1]), atom.get_full_id()[2], sum_ocu))
+                self.tx.configure(state='disabled')
 
 
 def main():
-    global root
-    global tx
-    global var1
-    root = tk.Tk()
-    root.title('Occucheck 2')
-    root.resizable(False, False)
-    m = tk.Menu(root)  # создается объект Меню на главном окне
-    root.config(menu=m)  # окно конфигурируется с указанием меню для него
-    fm = tk.Menu(m)  # создается пункт меню с размещением на основном меню (m)
-    # пункту располагается на основном меню (m)
-    m.add_cascade(label='Файл', menu=fm)
-    # формируется список команд пункта меню
-    fm.add_command(label='Открыть PDB', command=open_pdb)
-    fm.add_command(label='Открыть CIF', command=open_cif)
-    fm.add_command(label='Открыть ID PDB', command=open_url)
-    fm.add_command(label='Сохранить LOG', command=save_log)
-    fm.add_command(label='Выход', command=close_win)
-    m.add_command(label='Запуск...', command=check_pdb)
-    m.add_command(label='Справка', command=about)
-    fra1 = ttk.Frame(root)
-    fra1.pack(fill=tk.X)
-    var1 = tk.IntVar()
-    sca1 = tk.Scale(fra1, orient='horizontal', length=560, from_=10,
-                    to=100, tickinterval=5, resolution=1, variable=var1)
-    sca1.grid(row=1, column=0, padx=5)
-    lab1 = ttk.Label(fra1, text=' Минимальная сумма заселенности (%): ')
-    lab1.grid(row=0, column=0, sticky='W', padx=5)
-    tx = tk.Text(root, width=80, height=10)
-    scr = ttk.Scrollbar(root, command=tx.yview)
-    tx.configure(yscrollcommand=scr.set, state='disabled')
-    tx.pack(side=tk.LEFT)
-    scr.pack(side=tk.RIGHT, fill=tk.Y)
-    root.mainloop()
+    app = App()
+    app.mainloop()
 
 
 if __name__ == '__main__':
--- occucheck_tk.py
+++ occucheck_tk.py
@@ -15,141 +15,152 @@ from tkinter.messagebox import showerror
 from tkinter.messagebox import showinfo
 
 
-def open_pdb():
-    global lines_pdb
-    opt = {'filetypes': [
-        ('Файлы PDB', ('.pdb', '.PDB', '.ent')), ('Все файлы', '.*')]}
-    pdb = askopenfilename(**opt)
-    if pdb:
-        try:
-            with open(pdb, 'r') as f:
-                lines_pdb = f.readlines()
-        except FileNotFoundError:
-            return
-        except UnicodeDecodeError:
-            showerror('Ошибка', 'Некорректный PDB файл!')
-            return
+class Gui(tk.Tk):
+    """ГУЙ"""
+
+    def __init__(self):
+        super().__init__()
+        self.title('Occucheck')
+        self.resizable(False, False)
+        self.protocol('WM_DELETE_WINDOW', self.close_win)
+        fra1 = ttk.Frame(self)
+        fra1.pack(fill=tk.X)
+        self.var1 = tk.IntVar()
+        sca1 = tk.Scale(fra1, orient='horizontal', length=560, from_=10,
+                        to=100, tickinterval=5, resolution=1, variable=self.var1)
+        sca1.grid(row=1, column=0, padx=5)
+        lab1 = ttk.Label(fra1, text='Минимальная сумма заселенности (%):')
+        lab1.grid(row=0, column=0, sticky='W', padx=5)
+        self.tx = tk.Text(self, width=80, height=10)
+        scr = ttk.Scrollbar(self, command=self.tx.yview)
+        self.tx.configure(yscrollcommand=scr.set, state='disabled')
+        self.tx.pack(side=tk.LEFT)
+        scr.pack(side=tk.RIGHT, fill=tk.Y)
+        self.menu()
+
+    def menu(self):
+        m = tk.Menu(self)  # создается объект Меню на главном окне
+        self.config(menu=m)  # окно конфигурируется с указанием меню для него
+        fm = tk.Menu(m)  # создается пункт меню с размещением на основном меню (m)
+        # пункту располагается на основном меню (m)
+        m.add_cascade(label='Файл', menu=fm)
+        # формируется список команд пункта меню
+        fm.add_command(label='Открыть PDB', command=self.open_pdb)
+        fm.add_command(label='Сохранить LOG', command=self.save_log)
+        fm.add_command(label='Выход', command=self.close_win)
+        m.add_command(label='Запуск...', command=self.check_pdb)
+        m.add_command(label='Справка', command=self.about)
+
+    def close_win(self):
+        if askyesno('Выход', 'Вы точно хотите выйти?'):
+            self.destroy()
+
+    @staticmethod
+    def about():
+        showinfo('Информация',
+                 'Проверка корректности суммы заселенностей атомов а.о.')
 
 
-def save_log():
-    sa = asksaveasfilename()
-    if sa:
-        letter = tx.get(1.0, tk.END)
-        try:
-            with open(sa, 'w') as f:
-                f.write(letter)
-        except FileNotFoundError:
-            return
+class App(Gui):
+    """Класс логики работы программы"""
 
+    def __init__(self):
+        super().__init__()
+        self.lines_pdb = None
 
-def close_win():
-    if askyesno('Выход', 'Вы точно хотите выйти?'):
-        root.destroy()
-
-
-def about():
-    showinfo('Информация',
-             'Проверка корректности суммы заселенностей атомов а.о.')
-
-
-def check_occupancy(atom, occupancy, resn, chain_id, res_name):
-    min_ocu = (var1.get()) / 100
-    atom_uniq = [e for i, e in enumerate(atom) if e not in atom[:i]]
-    for x in atom_uniq:
-        i = [index for index, val in enumerate(atom) if val == x]
-        occupancy2 = []
-        atom3 = []
-        res_name2 = []
-        for n in i:
-            occupancy2.append(occupancy[n])
-            atom3.append(atom[n])
-            res_name2.append(res_name[n])
-        if (sum(occupancy2) > 1.00) or (sum(occupancy2) < min_ocu):
-            tx.configure(state='normal')
-            tx.insert(tk.INSERT, ('Для атома {0:s} а.о. {1:s}:{2:4d} цепи {3:s} cумма'
-                                  ' заселенностей равна {4:.2f}\n').format(
-                ''.join(set(atom3)), ' '.join(set(res_name2)), resn, chain_id, sum(occupancy2)))
-            tx.configure(state='disabled')
-            root.update()
-        del occupancy2
-        del atom3
-        del res_name2
-
-
-def check_pdb():
-    try:
-        if len(lines_pdb) < 10:
-            showerror('Ошибка', 'Некорректный PDB файл!')
-            return
-    except NameError:
-        showerror('Ошибка', 'Не загружен PDB файл!')
-        return
-    tx.delete(1.0, tk.END)
-    atom = []
-    occupancy = []
-    res_name = []
-    n = 0
-    while n < len(lines_pdb) - 1:
-        s = lines_pdb[n]
-        if (s[0:6] == 'HETATM') or (s[0:6] == 'ATOM  '):
+    def open_pdb(self):
+        opt = {'filetypes': [
+            ('Файлы PDB', ('.pdb', '.PDB', '.ent')), ('Все файлы', '.*')]}
+        pdb = askopenfilename(**opt)
+        if pdb:
             try:
-                resn_curent = int(s[22:26])
-            except ValueError:
+                with open(pdb, 'r') as f:
+                    self.lines_pdb = f.readlines()
+            except FileNotFoundError:
+                return
+            except UnicodeDecodeError:
                 showerror('Ошибка', 'Некорректный PDB файл!')
                 return
-            chain_id_curent = str(s[21])
-            while n < len(lines_pdb) - 1:
-                if (s[0:6] == 'HETATM') or (s[0:6] == 'ATOM  '):
-                    resn = int(s[22:26])
-                    chain_id = str(s[21])
-                    if (chain_id != chain_id_curent) or (resn_curent != resn):
-                        check_occupancy(atom, occupancy,
-                                        resn_curent, chain_id_curent, res_name)
-                        n -= 1
-                        atom.clear()
-                        occupancy.clear()
-                        res_name.clear()
-                        break
-                    atom.append(str(s[12:16]))
-                    occupancy.append(float(s[54:60]))
-                    res_name.append(str(s[17:20]))
-                n += 1
-                s = lines_pdb[n]
-        n += 1
+
+    def save_log(self):
+        sa = asksaveasfilename()
+        if sa:
+            letter = self.tx.get(1.0, tk.END)
+            try:
+                with open(sa, 'w') as f:
+                    f.write(letter)
+            except FileNotFoundError:
+                return
+
+    def check_occupancy(self, atom, occupancy, resn, chain_id, res_name):
+        min_ocu = (self.var1.get()) / 100
+        atom_uniq = [e for i, e in enumerate(atom) if e not in atom[:i]]
+        for x in atom_uniq:
+            i = [index for index, val in enumerate(atom) if val == x]
+            occupancy2 = []
+            atom3 = []
+            res_name2 = []
+            for n in i:
+                occupancy2.append(occupancy[n])
+                atom3.append(atom[n])
+                res_name2.append(res_name[n])
+            if (sum(occupancy2) > 1.00) or (sum(occupancy2) < min_ocu):
+                self.tx.configure(state='normal')
+                self.tx.insert(tk.INSERT, ('Для атома {0:s} а.о. {1:s}:{2:4d} цепи {3:s} cумма'
+                                           ' заселенностей равна {4:.2f}\n').format(
+                    ''.join(set(atom3)), ' '.join(set(res_name2)), resn, chain_id, sum(occupancy2)))
+                self.tx.configure(state='disabled')
+                self.update()
+            del occupancy2
+            del atom3
+            del res_name2
+
+    def check_pdb(self):
+        if self.lines_pdb is None:
+            showerror('Ошибка', 'Не загружен PDB файл!')
+            return
+        if len(self.lines_pdb) < 10:
+            showerror('Ошибка', 'Некорректный PDB файл!')
+            return
+        self.tx.configure(state='normal')
+        self.tx.delete('1.0', tk.END)
+        self.tx.configure(state='disabled')
+        atom = []
+        occupancy = []
+        res_name = []
+        n = 0
+        while n < len(self.lines_pdb) - 1:
+            s = self.lines_pdb[n]
+            if (s[0:6] == 'HETATM') or (s[0:6] == 'ATOM  '):
+                try:
+                    resn_curent = int(s[22:26])
+                except ValueError:
+                    showerror('Ошибка', 'Некорректный PDB файл!')
+                    return
+                chain_id_curent = str(s[21])
+                while n < len(self.lines_pdb) - 1:
+                    if (s[0:6] == 'HETATM') or (s[0:6] == 'ATOM  '):
+                        resn = int(s[22:26])
+                        chain_id = str(s[21])
+                        if (chain_id != chain_id_curent) or (resn_curent != resn):
+                            self.check_occupancy(atom, occupancy,
+                                                 resn_curent, chain_id_curent, res_name)
+                            n -= 1
+                            atom.clear()
+                            occupancy.clear()
+                            res_name.clear()
+                            break
+                        atom.append(str(s[12:16]))
+                        occupancy.append(float(s[54:60]))
+                        res_name.append(str(s[17:20]))
+                    n += 1
+                    s = self.lines_pdb[n]
+            n += 1
 
 
 def main():
-    global root
-    global tx
-    global var1
-    root = tk.Tk()
-    root.title('Occucheck')
-    root.resizable(False, False)
-    m = tk.Menu(root)  # создается объект Меню на главном окне
-    root.config(menu=m)  # окно конфигурируется с указанием меню для него
-    fm = tk.Menu(m)  # создается пункт меню с размещением на основном меню (m)
-    # пункту располагается на основном меню (m)
-    m.add_cascade(label='Файл', menu=fm)
-    # формируется список команд пункта меню
-    fm.add_command(label='Открыть PDB', command=open_pdb)
-    fm.add_command(label='Сохранить LOG', command=save_log)
-    fm.add_command(label='Выход', command=close_win)
-    m.add_command(label='Запуск...', command=check_pdb)
-    m.add_command(label='Справка', command=about)
-    fra1 = ttk.Frame(root)
-    fra1.pack(fill=tk.X)
-    var1 = tk.IntVar()
-    sca1 = tk.Scale(fra1, orient='horizontal', length=560, from_=10,
-                    to=100, tickinterval=5, resolution=1, variable=var1)
-    sca1.grid(row=1, column=0, padx=5)
-    lab1 = ttk.Label(fra1, text='Минимальная сумма заселенности (%):')
-    lab1.grid(row=0, column=0, sticky='W', padx=5)
-    tx = tk.Text(root, width=80, height=10)
-    scr = ttk.Scrollbar(root, command=tx.yview)
-    tx.configure(yscrollcommand=scr.set, state='disabled')
-    tx.pack(side=tk.LEFT)
-    scr.pack(side=tk.RIGHT, fill=tk.Y)
-    root.mainloop()
+    app = App()
+    app.mainloop()
 
 
 if __name__ == '__main__':
